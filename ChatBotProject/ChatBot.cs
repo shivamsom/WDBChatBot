4ad396791891e8ec810ea0ec3a8762996a9952cb
unknown <shivamsom3@gmail.com>
2018-03-29 12:20:54 +0530

    Shivam-AIML SEPARATED

---
+++ ChatBotProject/ChatBot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIMLbot;
+
+namespace WDB
+{
+    public partial class ChatBot
+    {
+        const string UserId = "szabist";
+        private Bot AimlBot;
+        private User myUser;
+
+        public ChatBot()
+        {
+            //AimlBot = new Bot();
+            //myUser = new User(UserId, AimlBot);
+            //Initialize();
+        }
+       
+        
+       
+       
+            
+            //  Loads all the AIML files in the \AIML folder         
+        public void Initialize()
+        {
+            //    AimlBot.loadSettings();
+            //    AimlBot.isAcceptingUserInput = false;
+            //    AimlBot.loadAIMLFromFiles();
+            //    AimlBot.isAcceptingUserInput = true;
+            //
+        }
+
+        // Given an input string, finds a response using AIMLbot lib
+        public String getOutput(String input)
+        {
+
+            // Luis
+            // Webclient Intent ->cooking
+
+            //Request r = new Request(input, myUser, AimlBot);
+            //Result res = AimlBot.Chat(r);
+            return "BIMBO";
+        }
+    }
+}
---
+++ ChatBotProject/bubble.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WDB
+{
+    partial class bubble
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblmessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblmessage.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmessage.ForeColor = System.Drawing.Color.White;
+            this.lblmessage.Location = new System.Drawing.Point(27, 50);
+            this.lblmessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(68, 38);
+            this.lblmessage.TabIndex = 2;
+            this.lblmessage.Text = "I\'m a Barbie girl in the Barbie world. Life in plastic, it\'s fantastic! You can b" +
+    "rush my hair, undress me everywhere. Imagination, life is your creation. Come on" +
+    ", Barbie, let\'s go party!";
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 40;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bubble
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.lblmessage);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "bubble";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 6);
+            this.Size = new System.Drawing.Size(116, 94);
+            this.Load += new System.EventHandler(this.bubble_Load);
+            this.Resize += new System.EventHandler(this.bubble_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblmessage;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+    }
+}
